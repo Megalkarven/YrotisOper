fc535ea27e2acd402119581c9302cbec10050b88
Megalkarven <44926505+Megalkarven@users.noreply.github.com>
2018-11-15 16:58:06 +0100

    Add files via upload

---
+++ Kings.java
@@ -0,0 +1,13 @@
+
+public class Kings {
+
+	public static void main(String[] args) {
+		System.out.println("##################################");
+		System.out.println("# Gorm den gamle   #      # 958  #");
+		System.out.println("# Harald Blatand   #      # 987  #");
+		System.out.println("# Svend Tveskeg    #      # 1014 #");
+		System.out.println("# Harald d. 2.     # 1014 # 1018 #");
+		System.out.println("# Knud den Store   # 1018 # 1035 #");
+		System.out.println("##################################");
+	}
+}
